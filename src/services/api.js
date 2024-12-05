f237bd3dce0596f28675d87e6845288921fca56a
Arpita Panigrahi <arpitapanigrahip@gmail.com>
2024-12-05 19:21:13 +0530

    Initial Commit

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = "d4fd2508384f8c3d205df999ae0204a1";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async () => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+    )}`
+);
+    const data = await response.json()
+    return data.results
+};
